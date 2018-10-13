f5fe2f5d3891a3608e89da828d17045882fb03a3
Serg <serg@auchat.ru>
2018-10-13 17:15:07 +0300

    upd

--- src/main.js
+++ src/main.js
@@ -1,20 +1,9 @@
 import App from './App.html';
 
-var  pars = (() => {
-	var  p = {};
-	location.search.substr(1).split('&').forEach((it) => {
-		var  arr = it.split('=');
-		p[arr[0]] = arr[1];
-	});
-	return p;
-})();
-
 var app = new App({
 	target: document.body,
 	//target: document.getElementsByClassName('editor-sidebarContainer')[0] || document.body,
 	data: {
-		urlParams: pars,
-		name: 'world'
 	}
 });
 
